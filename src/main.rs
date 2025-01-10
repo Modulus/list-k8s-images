b1510ae93a406d74c67de1970a546b6b01e51205
John Sigvald Skauge <john.sigvald.skauge@dnb.no>
2025-01-10 14:00:24 +0100

    Listing deployments as well

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,34 @@
-use k8s_openapi::api::core::v1::Pod;
+use k8s_openapi::{api::{apps::v1::Deployment, core::v1::Pod}, serde::de};
 use kube::api::{Api, ListParams, ObjectList};
 #[tokio::main]
 async fn main() -> Result<(), kube::Error> {
     let client = kube::Client::try_default().await?;
 
-    let pods: Api<Pod> = Api::namespaced(client, "odm-test");
+    let ns: Api<Pod> = Api::namespaced(client.clone(), "odm-test");
     let params = ListParams::default().labels("app.kubernetes.io/instance=odm-test");
-    let pods :ObjectList<Pod> = pods.list(&params).await?;
+    let pods :ObjectList<Pod> = ns.list(&params).await?;
 
-    println!("{:?}", pods);
-
-
+    // println!("{:?}", pods);
+    println!("Pods in the cluster in ns odm-test");
     for pod in pods {
         println!("{:?}", pod.metadata.name.unwrap());
     }
+
+    println!("\n\n");
+
+    let ns: Api<Deployment> = Api::namespaced(client.clone(), "odm-test");
+    let params = ListParams::default().labels("release=odm-test");
+    let deployments :ObjectList<Deployment> = ns.list(&params).await?;
+
+
+
+    println!("Deploymens in the cluster in ns odm-test");
+    for deploy in deployments {
+        println!("{:?}", deploy.metadata.name.unwrap());
+    }
+
+
+
 
     Ok(())
 }
